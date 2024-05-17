97da4e203fc24fe9e7c0e5081347fd3146c58902
Mamed <atalimon1399@gmail.com>
2024-05-17 09:00:17 +0800

    Lets go

---
+++ Django-TODO-Project-master/profiles/forms.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
+from django import forms
+from .models import CustomUser
+from django.core.exceptions import ValidationError
+
+
+class CustomUserCreationForm(UserCreationForm):
+    user_name = forms.CharField(max_length=30,error_messages={'required':'please enter a valid username'})
+    email = forms.EmailField(max_length=30 ,
+        error_messages={'required':'please enter a valid email'})
+    
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
+
+    class Meta:
+        model = CustomUser
+        fields = ( 'email', 'user_name' ,'password1', 'password2')
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ('email',)
+
+class CustomAuthForm(AuthenticationForm):
+    username = forms.EmailField() 
+    password = forms.CharField()
+
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'password']
