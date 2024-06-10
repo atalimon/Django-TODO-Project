ee23b49cd8ba9396f20c4c0385f7a9e65eac821f
Mamed <atalimon1399@gmail.com>
2024-06-10 15:46:37 +0800

    tempcheck

--- AtaDjango/settings.py
+++ AtaDjango/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
+#SECRET_KEY = 'django-insecure-p)!0!46dp&wgt!r9w%h6(d3^(17k3!sl^%j%8*-#vu-f)og$0&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'True'
 #DEBUG =  True
